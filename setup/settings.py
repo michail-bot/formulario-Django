6178b02b7072f6abf2e28dade2260274828ca3dc
michail salata <michailsalata@gmail.com>
2022-08-22 15:19:59 -0300

    novos campos

--- setup/settings.py
+++ setup/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tempus_dominus',
     'passagens',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+TEMPUS_DOMINUS_LOCALIZE = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
